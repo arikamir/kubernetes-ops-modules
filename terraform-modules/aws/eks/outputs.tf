e67b30fb2fe838e998a45e428c1b4cf8e1502b53
arik amir <arikamir1+github@gmail.com>
2023-09-17 20:03:52 +0300

    aws_auth_configmap_yaml

--- terraform-modules/aws/eks/outputs.tf
+++ terraform-modules/aws/eks/outputs.tf
@@ -50,5 +50,10 @@ output "eks_managed_node_groups_arns" {
       rolearn  = item.iam_role_arn
     }
   ]
+
+output "aws_auth_configmap_yaml" {
+  description = "Formatted yaml output for base aws-auth configmap containing roles used in cluster node groups/fargate profiles"
+  value       = module.eks.aws_auth_configmap_yaml
+}
 }
 
